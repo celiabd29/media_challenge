7d09530cf25b3c7521f8542d5b649be6b06f1848
celiabd29 <c_abbad@stu-digital-campus.fr>
2025-06-27 20:36:18 +0200

    video

---
+++ src/app/modifier/podcast/[id]/page.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function ModifierPodcastPage() {
+  return (
+    <div>
+      <h1>Modifier le podcast</h1>
+    </div>
+  );
+}
---
+++ src/app/modifier/video/[id]/page.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function ModifierVideoPage() {
+  return (
+    <div>
+      <h1>Modifier la page video</h1>
+    </div>
+  );
+}
